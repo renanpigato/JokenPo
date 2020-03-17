c8ca71aa8a417ed43f1331c45bbf3bcaa57dde9c
renan@renanpigato.inf.br <renan@renanpigato.inf.br>
2020-03-17 12:34:59 -0300

    Encapsulado lógica em outros métodos

--- src/Judge.java
+++ src/Judge.java
@@ -1,5 +1,5 @@
 import java.util.ListIterator;
-import itens.ItemInteface;
+import itens.ItemInterface;
 
 public class Judge {
 
@@ -12,139 +12,14 @@ public class Judge {
 	 * @param players
 	 */
 	public Judge(Game game) {
-		super();
 		this.players = game.getPlayers();
 	}
 
 	/**
-	 * Judge a game with the players of instance
-	 * 
-	 * @return
-	 */
-	public int adjudicate() {
-		return this.adjudicate(this.players);
-	}
-	
-	/**
-	 * Judge a game with the players informed
-	 * 
-	 * @param players
-	 * @return
-	 */
-	public int adjudicate(PlayersList players) {
-		
-		ListIterator<Player> itPlayers = players.listIterator();
-		
-		while (itPlayers.hasNext()) {
-			
-			Player player  = (Player) itPlayers.next();
-			ItemInteface itemWin   = player.getItem().getWinTo();
-			ItemInteface itemLoose = player.getItem().getLooseTo();
-			
-			ListIterator<Player> itAnotherPlayers = players.listIterator();
-			
-			while (itAnotherPlayers.hasNext()) {
-				
-				Player anotherPlayer = (Player) itAnotherPlayers.next();
-				
-				if(!player.equals(anotherPlayer)) {
-					
-					if(anotherPlayer.getItem().typeOf().equals(itemWin.typeOf())) {
-						this.winners.remove(player);
-						this.winners.add(player);
-					}
-					
-					if(anotherPlayer.getItem().typeOf().equals(itemLoose.typeOf())) {
-						this.loosers.remove(player);
-						this.loosers.add(player);
-					}
-				}
-			}
-		}
-		
-		if(!this.loosers.isEmpty()) {
-			
-			ListIterator<Player> itLoosers = this.loosers.listIterator();
-			
-			while (itLoosers.hasNext()) {
-				
-				Player pLooser = (Player) itLoosers.next();
-				this.winners.remove(pLooser);
-			}
-		}
-		
-		if(this.winners.isEmpty()) {
-			return -1;
-		}
-		
-		if(this.winners.size() == 1) {
-			this.winner = this.winners.get(0);
-			return Game.WIN;
-		}
-		
-		return Game.TIE_GAME;
-	}
-	
-	/**
-	 * Process a result of a game
-	 * 
-	 * @return
-	 */
-	public String result() {
-		return this.processResult(this.players);
-	}
-	
-	/**
-	 * Process a result of a game
-	 * 
-	 * @return
-	 */
-	public String result(PlayersList players) {
-		return this.processResult(players);
-	}
-	
-	private String processResult(PlayersList players) {
-		
-		String result = new String("");
-		
-		switch (this.adjudicate(players)) {
-		
-			case Game.WIN:
-				result += this.getWinner().getName() + "("+ this.getWinner().getItem().translatePortuguese() +") ganhou o jogo";
-				break;
-				
-			case Game.TIE_GAME:
-				PlayersList winners = this.getWinners();
-				String winnersName  = new String("");
-				ListIterator<Player> itWinners = winners.listIterator();
-				
-				while (itWinners.hasNext()) {
-					
-					Player winner = (Player) itWinners.next();
-					winnersName  += winner.getName();
-					winnersName  += "("+ winner.getItem().translatePortuguese() +")";
-					
-					if(itWinners.hasNext()) {
-						winnersName += new String(", ");
-					}
-				}
-				
-				result += "Houve empate entre os jogadores: "+ winnersName;
-				break;
-	
-			default:
-				result += "Não houve ganhadores";
-				break;
-		}
-		
-		return result;
-	}
-	
-	/**
 	 * @return the winner
 	 */
 	public Player getWinner() {
-		return winner;
+		return this.winner;
 	}
 
 	/**
@@ -177,5 +52,165 @@ public class Judge {
 		}
 		
 		return false;
+	}
+	
+	/**
+	 * Judge a game with the players of instance
+	 * 
+	 * @return
+	 */
+	public int adjudicate() {
+		return this.adjudicate(this.players);
+	}
+	
+	
+	/**
+	 * Method that separate loosers and winners
+	 * 
+	 * @param player
+	 * @param anotherPlayer
+	 */
+	private void detachWinnersLoosers(Player player, Player anotherPlayer) {
+		
+		ItemInterface itemWin   = player.getItem().getWinTo();
+		ItemInterface itemLoose = player.getItem().getLooseTo();
+		
+		if(anotherPlayer.getItem().typeOf().equals(itemWin.typeOf())) {
+			this.winners.remove(player);
+			this.winners.add(player);
+		}
+		
+		if(anotherPlayer.getItem().typeOf().equals(itemLoose.typeOf())) {
+			this.loosers.remove(player);
+			this.loosers.add(player);
+		}
+	}
+
+	/**
+	 * This method clean the winners 
+	 * that loose to another player
+	 */
+	private void cleanWinners() {
+		
+		if(!this.loosers.isEmpty()) {
+			
+			ListIterator<Player> itLoosers = this.loosers.listIterator();
+			
+			while (itLoosers.hasNext()) {
+				
+				Player pLooser = (Player) itLoosers.next();
+				this.winners.remove(pLooser);
+			}
+		}
+	}
+	
+	/**
+	 * Judge a game with the players informed
+	 * 
+	 * @param players
+	 * @return
+	 */
+	public int adjudicate(PlayersList players) {
+		
+		ListIterator<Player> itPlayers = players.listIterator();
+		
+		while (itPlayers.hasNext()) {
+			
+			Player player  = (Player) itPlayers.next();
+			ListIterator<Player> itAnotherPlayers = players.listIterator();
+			
+			while (itAnotherPlayers.hasNext()) {
+				
+				Player anotherPlayer = (Player) itAnotherPlayers.next();
+				
+				if(!player.equals(anotherPlayer)) {
+					this.detachWinnersLoosers(player, anotherPlayer);
+				}
+			}
+		}
+		
+		this.cleanWinners();
+		
+		if(this.winners.isEmpty()) {
+			return Game.LOOSE;
+		}
+		
+		if(this.winners.size() == 1) {
+			this.winner = this.winners.get(0);
+			return Game.WIN;
+		}
+		
+		return Game.TIE_GAME;
+	}
+	
+	/**
+	 * Process a result of a game
+	 * 
+	 * @return
+	 */
+	public String result() {
+		return this.processResult(this.players);
+	}
+	
+	/**
+	 * Process a result of a game
+	 * 
+	 * @return
+	 */
+	public String result(PlayersList players) {
+		return this.processResult(players);
+	}
+	
+	/**
+	 * Process and return the result of a judge
+	 * 
+	 * @param players
+	 * @return
+	 */
+	private String processResult(PlayersList players) {
+		
+		String result = new String("");
+		
+		switch (this.adjudicate(players)) {
+		
+			case Game.WIN:
+				result += this.getWinner().getName() + "("+ this.getWinner().getItem().translatePortuguese() +") ganhou o jogo";
+				break;
+				
+			case Game.TIE_GAME:
+				result += "Houve empate entre os jogadores: "+ this.getWinnersName();
+				break;
+	
+			default:
+				result += "Não houve ganhadores";
+				break;
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * Return the name of winners
+	 * 
+	 * @return
+	 */
+	public String getWinnersName() {
+		
+		PlayersList winners = this.getWinners();
+		String winnersName  = new String("");
+		ListIterator<Player> itWinners = winners.listIterator();
+		
+		while (itWinners.hasNext()) {
+			
+			Player winner = (Player) itWinners.next();
+			winnersName  += winner.getName();
+			winnersName  += "("+ winner.getItem().translatePortuguese() +")";
+			
+			if(itWinners.hasNext()) {
+				winnersName += new String(", ");
+			}
+		}
+		
+		return winnersName;
 	}
 }
